50de5e8da292cfdc010d2060e9075414521974bc
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2025-04-21 18:53:00 +0300

    import and build webgl

--- Assets/Scripts/Action/Obstacle.cs
+++ Assets/Scripts/Action/Obstacle.cs
@@ -1,22 +1,25 @@
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
+
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
 
     public int layerGroup = 1;
-    
-    [SerializeField]private SpriteRenderer _sr;
-   [SerializeField] private Collider2D _collider;
+
+    [SerializeField] private SpriteRenderer _sr;
+    [SerializeField] private Collider2D _collider;
 
     void Awake()
     {
         _sr = GetComponent<SpriteRenderer>();
         _collider = GetComponent<Collider2D>();
-        
+
         _sr.sortingLayerName = "Game";
         _sr.sortingOrder = layerGroup;
-        
+
         _collider.enabled = false;
     }
 
@@ -30,7 +33,7 @@ public class Obstacle : MonoBehaviour
         print("Clickeddddd");
         GameManager.Instance.RemoveObstacle(this);
     }
-
+#if UNITY_EDITOR
     void OnDrawGizmos()
     {
         if (_sr != null)
@@ -39,4 +42,5 @@ public class Obstacle : MonoBehaviour
             Handles.Label(transform.position, $"Layer: {layerGroup}\nOrder: {_sr.sortingOrder}");
         }
     }
+    #endif
 }
